f85a8ade4de49b4eba28e4a25beb8864f8d09501
parth-io <67057645+parth-io@users.noreply.github.com>
2020-08-08 12:28:09 +0800

    some_message

--- Project to Sort My Lists/Solution.java
+++ Project to Sort My Lists/Solution.java
@@ -3,6 +3,7 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.lang.*;
+import java.lang.reflect.Method;
 import java.time.*;
 import java.util.regex.*;
 import java.security.*;
@@ -14,25 +15,28 @@ import static java.util.stream.Collectors.toList;
 
 public class Solution {
 
-    static Pattern p_Trailing_Words = Pattern.compile("(\\r\\n|[ \\t\\x0B\\f\\n\\r\\u2028\\u2029\\u0085])+");
-    
-    static Pattern p_Trailing_Phrases = Pattern.compile("^(\\r\\n|[ \\t\\x0B\\f\\n\\r\\u2028\\u2029\\u0085])+|(\\r\\n|[ \\t\\x0B\\f\\n\\r\\u2028\\u2029\\u0085])+$");
+    static Pattern p_Trailing = Pattern.compile("^(\\r\\n|[ \\t\\x0B\\f\\n\\r\\u2028\\u2029\\u0085])+|(\\r\\n|[ \\t\\x0B\\f\\n\\r\\u2028\\u2029\\u0085])+$");
 
     //Removes trailing whitespace and stuff
-    public static String remove_Trailing(String s, Pattern p_Trailing) {
+    public static String remove_Trailing(String s) {
         Matcher m = p_Trailing.matcher(s);
         return m.replaceAll("");
     }
     
     //Prints items from a set
-    public static void printer(Set<String> set) {
+    //Why 131072? It is a multiple of 512, the disk sector size for my HDD and is 128K
+    public static void printer(Set<String> set, String filename) throws IOException {
+        String name = filename + ".txt";
+        BufferedWriter print = new BufferedWriter(new FileWriter(name), 131072);
         for(String d : set) {
-            System.out.println(d);
+            print.write(d + "\n");
         }
+        print.flush();
+        print.close();
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader scan = new BufferedReader(new InputStreamReader(System.in), 131072);
         
         Set<String> phrases = new HashSet<String>();
         
@@ -42,21 +46,30 @@ public class Solution {
         
         Pattern p = Pattern.compile(". .", Pattern.MULTILINE);
         
+        Pattern p_Corner_Case = Pattern.compile("\\w$");
+        
         while(!(s.equals("EOF"))) {
+            s = remove_Trailing(s);
             Matcher m = p.matcher(s);
             if(m.find()) {
-                phrases.add(remove_Trailing(s, p_Trailing_Phrases));
+                phrases.add(s);
             }
             else {
-                words.add(remove_Trailing(s, p_Trailing_Words));
+                Matcher m_Corner_Case = p_Corner_Case.matcher(s);
+                if (m_Corner_Case.find()) {
+                    words.add(s);
+                }
+                else {
+                    phrases.add(s);
+                }
             }
             s = scan.readLine();
         }
         
         scan.close();
         
-        printer(words);
+        printer(words, "words");
         
-        printer(phrases);
+        printer(phrases, "phrases");
     }
 }
