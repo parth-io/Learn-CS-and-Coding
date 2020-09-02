691ff031889e9641beabe6f7b5bcb43accb83c8c
parth-io <67057645+parth-io@users.noreply.github.com>
2020-09-03 02:11:00 +0800

    some_message

--- Bash Scripting/lol.sh
+++ Bash Scripting/lol.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #cd /media/parth/My\ Passport/Videos/Movies/English/
-cd /media/parth/.BIN/Videos/TV Shows/Classics/
+cd /media/parth/.BIN/Videos/English/#To\ Watch/
 pwd
 ls > del.txt
 printf "Done"
