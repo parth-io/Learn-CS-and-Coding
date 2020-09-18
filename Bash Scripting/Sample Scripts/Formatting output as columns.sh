b5a9c5dc2bbf2e0be39fa295dcad8c7cf279fd8a
parth-io <67057645+parth-io@users.noreply.github.com>
2020-09-18 22:34:21 +0800

    Update and rename c to Formatting output as columns.sh

---
+++ Bash Scripting/Sample Scripts/Formatting output as columns.sh
@@ -0,0 +1,55 @@
+From - https://stackoverflow.com/questions/26130909/bash-output-column-formatting
+Other variants - https://unix.stackexchange.com/questions/59249/print-output-to-3-separate-columns
+https://stackoverflow.com/questions/6462894/how-can-i-format-the-output-of-a-bash-command-in-neat-columns
+
+#Script 1
+#!/bin/bash
+file="/Users/USER12/Desktop/url-list.txt"
+log="/Users/USER12/Desktop/url-results.txt"
+fmt="%-25s%-12s%-12s\n"
+printf "$fmt" DOMAIN_NAME HTTP_CODE RESPONSE_TIME > "$log"
+while read line
+do
+    read code time < <(curl -o /dev/null --silent --head --write-out '%{http_code} %{time_total}' "$line")
+    printf "$fmt" "$line" "$code" "$time" >> "$log"
+done <"$file"
+
+#Script 2 - more concise version
+#!/bin/bash
+
+file="./url.txt"
+fmt="%s\t%s\t%s\n"
+( printf "$fmt" "DOMAIN_NAME" "HTTP_CODE" "RESPONSE_TIME"
+while read -r line
+do
+    printf "$fmt" "$line" $(curl -o /dev/null --silent --head --write-out '%{http_code} %{time_total}' "$line")
+done <"$file" ) | column -t > ./out.txt
+
+#The above 2 codes open and close the log file with each loop. This can be avoided by using exec to redirect stdout to the log file before the first printf statement.
+
+#Script 3
+#!/bin/bash
+file="/Users/USER12/Desktop/url-list.txt"
+exec >"/Users/USER12/Desktop/url-results.txt"
+fmt="%-25s%-12s%-12s\n"
+printf "$fmt" DOMAIN_NAME HTTP_CODE RESPONSE_TIME
+while read line
+do
+    read code time < <(curl -o /dev/null --silent --head --write-out '%{http_code} %{time_total}' "$line")
+    printf "$fmt" "$line" "$code" "$time"
+done <"$file"
+
+#Or group the print statements, which also helpfully restores stdout to its normal value.
+
+#Script 4
+#!/bin/bash
+file="/Users/USER12/Desktop/url-list.txt"
+fmt="%-25s%-12s%-12s\n"
+{
+printf "$fmt" DOMAIN_NAME HTTP_CODE RESPONSE_TIME
+while read line
+do
+    read code time < <(curl -o /dev/null --silent --head --write-out '%{http_code} %{time_total}' "$line")
+    printf "$fmt" "$line" "$code" "$time"
+done <"$file"
+} >"/Users/USER12/Desktop/url-results.txt"
